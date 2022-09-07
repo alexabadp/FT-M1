23caf6d992d4d67a17d780ede5c74c69c55b7ed8
Alex <Aalex.abad96@gmail.com>
2022-09-07 17:26:57 -0500

    homework-01

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -3,11 +3,26 @@
 function BinarioADecimal(num) {
   // tu codigo aca
 
+  let acc = 0;
+
+  for (let i = 0; i < num.length; i++) {
+    acc += Math.pow(2,num.length-1-i)*num[i];  
+  }
+
+  return acc;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
 
+  let valor='';
+
+  while (num) {
+    valor = (num%2) + valor;
+    num = Math.floor(num / 2);
+  }
+
+  return valor;
 }
 
 
