3122e05320df0ddc2cc1926002262fed2d90cff8
Alex <Aalex.abad96@gmail.com>
2022-09-13 07:59:00 -0500

    commit 2

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -3,26 +3,44 @@
 function BinarioADecimal(num) {
   // tu codigo aca
 
-  let acc = 0;
+  // let acc = 0;
 
-  for (let i = 0; i < num.length; i++) {
-    acc += Math.pow(2,num.length-1-i)*num[i];  
+  // for (let i = 0; i < num.length; i++) {
+  //   acc += Math.pow(2,num.length-1-i)*num[i];  
+  // }
+
+  // return acc;
+  var array = num.split('');
+  var sum = 0;
+  for (var i = 0; i < array.length; i++) {
+    sum = sum + Math.pow(2, array.length - 1 - i) * array[i];
   }
-
-  return acc;
+  return sum;
 }
+
 
 function DecimalABinario(num) {
   // tu codigo aca
 
-  let valor='';
+  // let valor='';
+
+  // while (num) {
+  //   valor = (num%2) + valor;
+  //   num = Math.floor(num / 2);
+  // }
+
+  // return valor;
+
+  if (num <= 0) return '0';
+
+  var array = []
 
   while (num) {
-    valor = (num%2) + valor;
+    array.unshift(num % 2);
     num = Math.floor(num / 2);
   }
 
-  return valor;
+  return array.join('')
 }
 
 
